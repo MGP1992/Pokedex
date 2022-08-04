a48b7d852893695c1aa06c90d688e62d3ece8591
MGP1992 <mathewgparkinson@gmail.com>
2022-08-04 12:24:08 +0100

    Add type category

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,7 +9,7 @@ function App() {
     <>
       <Search pkmnName={'string'} />
       <div>{pkmnName}</div>
-      <Pkmninfo name="Zapdos" height="1.6m" weight="52.6kg" location="Cerulean city power plant"
+      <Pkmninfo name="Zapdos" type="electric/flying" height="1.6m" weight="52.6kg" location="Cerulean city power plant"
       description="Zapdos is a large, avian Pokémon with predominantly yellow plumage. It has small, triangular eyes with black around them and a long, thin, light orange beak. Its wings and tail are a mass of spiky feathers and there are black feathers covering the back of its wings and inner tail feathers. A large crest of spiked feathers surrounds its head. Its thighs are tan, and it has thin, light orange legs. It has feet with three forward-facing toes and one backward-facing toe; each toe has a short talon.
       When Zapdos flaps its glittering wings, it releases electricity that can potentially cause thunderstorms. It produces massive crackling and snapping sounds when it flies; these are attributed to the lightning bolts it sheds when airborne. Zapdos reportedly appears only during thunderstorms and gains power when struck by lightning. It is said to live among thunderclouds, but it is rarely seen."
       />
--- frontend/src/components/info.js
+++ frontend/src/components/info.js
@@ -4,6 +4,7 @@ function Pkmninfo(props) {
 
         <div>
             <h1>Pokedex entry: {props.name}</h1>
+            <h2>Type: {props.type}</h2>
             <h2>Height: {props.height}</h2>
             <h2>Weight: {props.weight}</h2>
             <h2>Pokemon location: {props.location}</h2>
