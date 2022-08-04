eb5985677f35994a9d428db44028cf00ae09cc96
MGP1992 <mathewgparkinson@gmail.com>
2022-08-04 12:06:37 +0100

    Add pokemon info component (hard coded)

---
+++ frontend/src/components/info.js
@@ -0,0 +1,17 @@
+function Pkmninfo(props) {
+    
+    return (
+
+        <div>
+            <h1>Pokedex entry: {props.name}</h1>
+            <h2>Height: {props.height}</h2>
+            <h2>Weight: {props.weight}</h2>
+            <h2>Pokemon location: {props.location}</h2>
+            <p>{props.description}</p>
+
+        </div>
+
+          
+    );
+  }
+export default Pkmninfo;
