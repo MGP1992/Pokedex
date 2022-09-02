53feab906f340855f9a6145be7d3721f4dc19909
MGP1992 <mathewgparkinson@gmail.com>
2022-09-02 11:10:34 +0100

    changed pokedex font

--- frontend/src/components/description.js
+++ frontend/src/components/description.js
@@ -4,7 +4,7 @@ function PkmnDescription(props) {
     return (
       <div id="pkmndescription">
           <img src = "https://c.tenor.com/p6qNTB18vuUAAAAM/zapdos-flying.gif" width="200px"/>
-          <p class="large-font">{props.description}</p>
+          <p>{props.description}</p>
       </div>         
     );
   }
