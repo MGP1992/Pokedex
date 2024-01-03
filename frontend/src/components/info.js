9d8d9d4475dbad76f622b7f4df19f9ec7d104fc9
MGP1992 <mathewgparkinson@gmail.com>
2024-01-03 13:50:53 +0000

    Edit height and weight for initial render

--- frontend/src/components/info.js
+++ frontend/src/components/info.js
@@ -6,8 +6,8 @@ function PkmnInfo(props) {
         <div id="pkmninfo" className="ml-14 mt-8 text-2xl max-w-lg">
             <h1>Pokédex entry: {_.capitalize(infoData?.name)}</h1>
             <h2>Type: {_.capitalize(infoData?.type)}</h2>
-            <h2>Height: {infoData?.height/10}m</h2>
-            <h2>Weight: {infoData?.weight/10}kg</h2>
+            <h2>Height: {infoData?.height? `${(infoData?.height/10)}m`:''}</h2>
+            <h2>Weight: {infoData?.weight? `${(infoData?.weight/10)}kg`:''}</h2>
             <h2>Pokemon location: {_.capitalize(infoData?.location.replace(/-/g, ' '))}</h2>
         </div>         
     );
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -8,7 +8,6 @@ ReactDOM.render(
     <div id="container" className="relative h-full w-full overflow-hidden">
       <img src={pokedexImage} alt="Pokedex" className="max-w-7xl max-h-7xl h-full w-full object-cover" />
       <div className="absolute top-0 left-0 right-0 bottom-0 overflow-hidden">
-        {/* Components rendered on top of the background image go here */}
         <App />
       </div>
     </div>
